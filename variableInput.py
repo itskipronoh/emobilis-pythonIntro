94ecf2e7dc04022a5eba2fd8186cf09cbabe949c
itskipronoh <kipronoyegon68@gmail.com>
2024-10-23 22:39:15 +0300

    second commit

--- variableInput.py
+++ variableInput.py
@@ -21,3 +21,25 @@ elif bmi <= 29.9:
     print("You are overweight.")
 else:
     print("You are obese.")
+
+
+withdrawal_amount = float(input("Enter the amount you want to withdraw: "))
+if withdrawal_amount > 10000:
+    discount_rate = 0.15
+elif withdrawal_amount > 5000:
+   discount_rate = 0.10
+elif withdrawal_amount > 2000:
+    discount_rate = 0.05
+else:
+    discount_rate = 0.00
+
+discount_amount = withdrawal_amount * discount_rate
+
+total_amount = withdrawal_amount - discount_amount
+
+print(f"Discount Awarded: {discount_rate:.2f}")
+print(f"Discount Amount: {discount_amount:.2f}")
+print(f"Total Amount: {total_amount:.2f}")
+print(f"Thank you for banking with us.")
+
+
